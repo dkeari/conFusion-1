65fef14b023479d663583b28dbacd1acc6fbfbb6
Douglas Keari <dkeari@TPA.LOCAL>
2020-09-22 23:59:01 +1000

    NPM Scripts Part 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,23 @@
 $(document).ready(function(){
-  $("#mycarousel").carousel({interval:5000});
-  $("#carousel-button").click(function(){
-     if ($("#carousel-button").children("span").hasClass('fa-pause')) {
-         $("#mycarousel").carousel('pause');
-         $("#carousel-button").children("span").removeClass('fa-pause');
-         $("#carousel-button").children("span").addClass('fa-play');
-     }
-     else if ($("#carousel-button").children("span").hasClass('fa-play')){
-         $("#mycarousel").carousel('cycle');
-         $("#carousel-button").children("span").removeClass('fa-play');
-         $("#carousel-button").children("span").addClass('fa-pause');
-     }
- });
-
-    $(document).ready(function() {
-
-        $("#loginpop").click(function() {
-        $("#loginModal").modal();
-        });
-        $(".close").click(function() {
-          $("#loginModal").modal('hide');
-        });
-        $("#logincancel").click(function() {
-          $("#loginModal").modal('hide');
-        });
-        $("#reserveButton").click(function() {
-          $("#reserveModal").modal();
-        });
-        $(".close").click(function() {
-          $("#reserveModal").modal('hide');
-        });
-        $("#reserveCancel").click(function() {
-          $("#reserveModal").modal('hide');
-        });
-
-      });
+  $("#mycarousel").carousel( { interval:2000 } );
+     $("#carouselButton").click(function(){
+      if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+          $("#mycarousel").carousel('pause');
+          $("#carouselButton").children("span").removeClass('fa-pause');
+          $("#carouselButton").children("span").addClass('fa-play');
+      }
+      else if ($("#carouselButton").children("span").hasClass('fa-play')){
+          $("#mycarousel").carousel('cycle');
+          $("#carouselButton").children("span").removeClass('fa-play');
+          $("#carouselButton").children("span").addClass('fa-pause');                    
+      }
+});	
+//Assignment 4 Task 1,2,3
+$("#myBtn").click(function(){
+$("#myModal").modal("toggle");
+});   
+//Assignment 4 Task 1
+$("#loginBtn").click(function(){
+$("#loginModal").modal("toggle");
+});
+});
